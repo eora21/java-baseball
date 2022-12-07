45b63b3071ce706fb0d73c25a2dd67c24add9752
eora21 <eora21@naver.com>
2022-12-07 17:41:27 +0900

    feat: 🎸 추가 게임 결과 메서드, 게임 종료 메서드

--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -1,5 +1,11 @@
 package baseball.view;
 
+import baseball.model.NumberRule;
+import baseball.model.PlayRule;
+
+import java.util.Map;
+import java.util.StringJoiner;
+
 public class Output {
     public void gameStart() {
         notice("숫자 야구 게임을 시작합니다.");
@@ -11,5 +17,19 @@ public class Output {
 
     private void notice(String text) {
         System.out.println(text);
+    }
+
+    public void gameResult(Map<PlayRule, Integer> result) {
+        StringJoiner stringJoiner = new StringJoiner(" ");
+
+        result.forEach((playRule, count) -> {
+            stringJoiner.add(String.format("%d%s", count, playRule.getDescription()));
+        });
+
+        notice(stringJoiner.toString());
+    }
+
+    public void gameOver() {
+        notice(String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", NumberRule.LENGTH.getValue()));
     }
 }
