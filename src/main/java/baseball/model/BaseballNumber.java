e29c45b892c4c278ba6b7feb7af7e740ca723ad1
eora21 <eora21@naver.com>
2022-12-07 16:37:39 +0900

    feat: 🎸 변경 BaseballNumber 생성을 정적 팩터리 메서드로 수정
    생성되는 메커니즘만 같을 뿐, 랜덤 생성과 사용자 지정 생성에 따른 클래스를 나눌 필요가 없다고 판단하였다.

--- src/main/java/baseball/model/BaseballNumber.java
+++ src/main/java/baseball/model/BaseballNumber.java
@@ -1,5 +1,8 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -7,19 +10,43 @@ import java.util.List;
 public class BaseballNumber {
     private final List<Integer> baseballNumber;
 
-    public BaseballNumber(List<Integer> numbers) {
-        validUnique(numbers);
-        validRange(numbers);
+    private BaseballNumber(List<Integer> numbers) {
         baseballNumber = numbers;
     }
 
-    private void validUnique(List<Integer> numbers) {
+    public static BaseballNumber createRandomPick() {
+        List<Integer> numbers = new ArrayList<>();
+        addUniqueNumber(numbers);
+        return new BaseballNumber(numbers);
+    }
+
+    public static BaseballNumber createUserPick(List<Integer> numbers) {
+        validUnique(numbers);
+        validRange(numbers);
+        return new BaseballNumber(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(baseballNumber);
+    }
+
+    private static void addUniqueNumber(List<Integer> numbers) {
+        while (numbers.size() < NumberRule.LENGTH.getValue()) {
+            int randomNumber = Randoms.pickNumberInRange(NumberRule.START.getValue(), NumberRule.END.getValue());
+
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
+
+    private static void validUnique(List<Integer> numbers) {
         if (new HashSet<>(numbers).size() != NumberRule.LENGTH.getValue()) {
             throw new IllegalArgumentException("서로 다른 수로 이루어지지 않았습니다.");
         }
     }
 
-    private void validRange(List<Integer> numbers) {
+    private static void validRange(List<Integer> numbers) {
         for (Integer number : numbers) {
             if (number < NumberRule.START.getValue() || NumberRule.END.getValue() < number) {
                 throw new IllegalArgumentException(
@@ -27,9 +54,5 @@ public class BaseballNumber {
                                 NumberRule.START.getValue(), NumberRule.END.getValue()));
             }
         }
-    }
-
-    public List<Integer> getNumbers() {
-        return Collections.unmodifiableList(baseballNumber);
     }
 }
