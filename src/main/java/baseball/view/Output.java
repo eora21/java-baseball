1bb013c4c0022f7274693f40208a5bc5705e0ae3
eora21 <eora21@naver.com>
2022-12-07 13:41:28 +0900

    feat: 🎸 추가 숫자 입력을 요구하는 메시지 출력 메서드

--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -5,6 +5,10 @@ public class Output {
         print("숫자 야구 게임을 시작합니다.");
     }
 
+    public void printRequireBaseballNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
     private void print(String text) {
         System.out.println(text);
     }
