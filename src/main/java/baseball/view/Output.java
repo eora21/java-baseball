c8cc6843148ba819e73cc7d51a9f90eb3167d956
eora21 <eora21@naver.com>
2022-12-07 13:36:57 +0900

    feat: 🎸 추가 야구 게임 시작 메시지 출력 메서드

---
+++ src/main/java/baseball/view/Output.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class Output {
+    public void printGameStart() {
+        print("숫자 야구 게임을 시작합니다.");
+    }
+
+    private void print(String text) {
+        System.out.println(text);
+    }
+}
