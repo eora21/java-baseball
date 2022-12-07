0c5368348f6c7cb0535a8066356a434c54d048ba
eora21 <eora21@naver.com>
2022-12-07 13:43:24 +0900

    refactor: 💡 변경 출력 메서드명을 print에서 notice로 수정
    System.out.print와 print의 메서드명이 중첩되어 명확하게 알아볼 수 있도록 수정

--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -2,14 +2,14 @@ package baseball.view;
 
 public class Output {
     public void printGameStart() {
-        print("숫자 야구 게임을 시작합니다.");
+        notice("숫자 야구 게임을 시작합니다.");
     }
 
     public void printRequireBaseballNumber() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    private void print(String text) {
+    private void notice(String text) {
         System.out.println(text);
     }
 }
